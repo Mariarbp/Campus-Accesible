482e1b2e358f484e5d092991dab589f61d4638d7
Rafa Mtz <rafaelmtz544@gmail.com>
2020-10-16 12:54:31 -0500

    Json ya jala

---
+++ CampusAccesible/CampusAccesible/Lugares.swift
@@ -0,0 +1,41 @@
+//
+//  Lugares.swift
+//  CampusAccesible
+//
+//  Created by Rafa Mtz on 16/10/20.
+//
+
+import UIKit
+
+class Lugares: NSObject, Codable {
+    
+    private var nombre : String
+    private var latitud : Float
+    private var longitud : Float
+    private var descripcion : String
+    private var baños : Bool!
+    private var elevadores : Bool!
+    
+    init(nombre : String, latitud : Float, longitud : Float, descripcion : String, baños : Bool, elevadores : Bool) {
+        self.nombre = nombre
+        self.latitud = latitud
+        self.longitud = longitud
+        self.descripcion = descripcion
+        self.baños = baños
+        self.elevadores = elevadores
+    }
+    
+    override init() {
+        nombre = ""
+        latitud = 0.0
+        longitud = 0.0
+        descripcion = ""
+        baños = false
+        elevadores = false
+    }
+    
+    func getNombre() -> String{
+        return nombre
+    }
+
+}
---
+++ CampusAccesible/CampusAccesible/helper.swift
@@ -0,0 +1,35 @@
+//
+//  helper.swift
+//  CampusAccesible
+//
+//  Created by Rafa Mtz on 16/10/20.
+//
+
+import Foundation
+
+class Helper{
+    
+    init() {
+        
+    }
+    
+    static let shared = Helper()
+    
+    func getInfoLugares() -> [Lugares] {
+        do {
+            if let path = Bundle.main.path(forResource: "Lugares", ofType: "json"),
+               let json = try String(contentsOfFile: path).data(using: .utf8){
+                do {
+                    let lugaresDecodificados = try JSONDecoder().decode([Lugares].self, from: json)
+                    return lugaresDecodificados
+                } catch  {
+                    return []
+                }
+            }
+        } catch  {
+            return []
+        }
+        return []
+    }
+    
+}
